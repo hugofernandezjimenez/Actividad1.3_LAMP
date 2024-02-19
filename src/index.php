cb8a00c1a072b84510b4ac5eb08fd2b566bca116
Ubuntu <ubuntu@ip-172-31-84-186.ec2.internal>
2024-02-19 18:15:18 +0000

    Tenia un archivo mal subido

--- src/index.php
+++ src/index.php
@@ -1,1 +1,9 @@
-echo "<?php phpinfo();?>">info.php
+<html>
+<head>
+
+	<title>Actividad 1.3.1</title>
+</head>
+<body>
+	<?php echo "<h1>Actividad 1.3.1: LAMP Server, Hugo Fernández Jiménez</h1>";?>
+</body>
+</html>
